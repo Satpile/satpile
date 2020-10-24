308f1e33290328bf43ff7a4b164aa61543bd02f9
4l3ss <2623600+4l3ss@users.noreply.github.com>
2020-10-24 18:57:19 +0200

    Upgrade to SDK39

--- utils/Bootstrap.ts
+++ utils/Bootstrap.ts
@@ -1,19 +1,19 @@
 import {Platform, UIManager} from "react-native";
-import {SplashScreen} from "expo";
+import * as SplashScreen from "expo-splash-screen";
 import * as TaskManager from "expo-task-manager";
 import {REFRESH_TASK} from "./Settings";
 import BalanceFetcher from "./BalanceFetcher";
 import {Notifications} from "./Notifications";
 
-export function bootstrap() {
+export async function bootstrap() {
   if (Platform.OS === 'android') {
     if (UIManager.setLayoutAnimationEnabledExperimental) {
       UIManager.setLayoutAnimationEnabledExperimental(true);
     }
   }
-  SplashScreen.preventAutoHide();
+  await SplashScreen.preventAutoHideAsync();
   if(!TaskManager.isTaskDefined(REFRESH_TASK)){
     TaskManager.defineTask(REFRESH_TASK, BalanceFetcher.backgroundFetch);
   }
-  Notifications.initNotifications();
+  await Notifications.initNotifications();
 }
