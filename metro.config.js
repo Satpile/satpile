65dd661ccd43e660ee5034d76bfc3a62b6eb9f5f
4l3ss <2623600+4l3ss@users.noreply.github.com>
2022-12-13 19:53:42 +0100

    Upgrade to SDK45

--- metro.config.js
+++ metro.config.js
@@ -1,8 +1,4 @@
-module.exports = {
-  transformer: {
-    assetPlugins: ['expo-asset/tools/hashAssetFiles'],
-  },
-  resolver: {
-    sourceExts: ['jsx', 'js', 'ts', 'tsx', 'cjs', 'json']
-  }
-};
+// Learn more https://docs.expo.io/guides/customizing-metro
+const { getDefaultConfig } = require('expo/metro-config');
+
+module.exports = getDefaultConfig(__dirname);
