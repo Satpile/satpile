ee1d2e973ed3d27bdb6d8f7a6037b78393daf466
4l3ss <2623600+4l3ss@users.noreply.github.com>
2023-07-08 22:32:06 +0200

    Improve version numbering

--- app.config.ts
+++ app.config.ts
@@ -5,11 +5,16 @@ export default ({ config }: ConfigContext): ExpoConfig => {
   const { name, slug } = config;
   invariant(name, "No name found in app.json");
   invariant(slug, "No slug found in app.json");
-
+  const appVersion = process.env.APP_VERSION || "0.0.0-0";
+  const [version, buildNumber] = appVersion.split("+");
   return {
     ...config,
     name,
     slug,
-    version: process.env.APP_VERSION || "0.0.0-dev",
+    ios: {
+      ...(config.ios || {}),
+      buildNumber: buildNumber || "1",
+    },
+    version,
   };
 };
