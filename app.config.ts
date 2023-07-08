c912d827dccf46650b4eb70ed38168aa53c96f2a
4l3ss <2623600+4l3ss@users.noreply.github.com>
2023-07-08 23:08:53 +0200

    Add dot env for version reading from .env

--- app.config.ts
+++ app.config.ts
@@ -1,11 +1,13 @@
 import { ExpoConfig, ConfigContext } from "expo/config";
 import invariant from "invariant";
-
+import dotenv from "dotenv";
+dotenv.config();
 export default ({ config }: ConfigContext): ExpoConfig => {
   const { name, slug } = config;
+  const appVersion = process.env.APP_VERSION;
   invariant(name, "No name found in app.json");
   invariant(slug, "No slug found in app.json");
-  const appVersion = process.env.APP_VERSION || "0.0.0+0";
+  invariant(appVersion, "No app version found in env");
   const [version, buildNumber] = appVersion.split("+");
   return {
     ...config,
