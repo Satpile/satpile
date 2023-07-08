7c1eab5cb515ef28a00a7dae7b03a30020e204c6
4l3ss <2623600+4l3ss@users.noreply.github.com>
2023-07-08 22:35:16 +0200

    Hotfix

--- app.config.ts
+++ app.config.ts
@@ -5,7 +5,7 @@ export default ({ config }: ConfigContext): ExpoConfig => {
   const { name, slug } = config;
   invariant(name, "No name found in app.json");
   invariant(slug, "No slug found in app.json");
-  const appVersion = process.env.APP_VERSION || "0.0.0-0";
+  const appVersion = process.env.APP_VERSION || "0.0.0+0";
   const [version, buildNumber] = appVersion.split("+");
   return {
     ...config,
